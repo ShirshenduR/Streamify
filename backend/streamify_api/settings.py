f1c4170d152180d169c13cfe3c9b31714445ce77
jyotisoni24 <144710804+jyotisoni24@users.noreply.github.com>
2025-10-21 20:51:48 +0530

    Update: Added Responsiveness in mini player section

--- backend/streamify_api/settings.py
+++ backend/streamify_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+
 from dotenv import load_dotenv
 load_dotenv()
 import dj_database_url
@@ -24,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-dev-secret-key")
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"]
