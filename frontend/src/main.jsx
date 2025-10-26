b44bd0f5fe7255afd96f8757ca2d87e4cc2c64f8
Md Tausif Iqbal <mdtausifiqbalnwd@gmail.com>
2025-10-26 15:04:17 +0530

    feat(ui): add heroui and tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
+import { HeroUIProvider } from '@heroui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <HeroUIProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HeroUIProvider>
   </StrictMode>,
 )
